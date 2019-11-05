586c06a0216cddbc3f3cb90c33545d8bc8b2f785
KPLUU <kph.luu@gmail.com>
2019-11-05 19:50:56 +0100

    working seed

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,56 @@
-class EventsController < ApplicationController
+class EventsController < ApplicationController 
   def index
+    @allevents = Event.all
+  end  
+  
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+      puts "$" * 60
+      puts "ceci est le contenu de params :"
+      puts params
+      @event = Event.new(start_date: params[:start_date], duration: params[:duration], title: params[:title], description: params[:description], price: params[:price], location: params[:location])
+      
+      #user_id: 11 correspond à l'ID anonymous après généré 10 Fakers Users via seed et créé manuellement l'user anonymous dans la console.
+
+      if @event.save # essaie de sauvegarder en base @gossip
+        # si ça marche, il redirige vers la page d'index du site
+        @event = Event.all
+        redirect_to root_path
+      else
+        # sinon, il render la view new (qui est celle sur laquelle on est déjà)
+        render :new
+      end
+
+  end
+
+  def edit
+    #@gossip = Gossip.find(params[:id])
+  end
+
+  def update
+    #@gossip = Gossip.find(params[:id])
+    #if @gossip.update(title: params[:title], content: params[:content])
+    #  redirect_to root_path
+    #else
+    #  render :edit
+    #end
+  end
+
+  def destroy
+    #@gossip = Gossip.find(params[:id])
+    #@gossip.destroy!
+    #redirect_to root_path, :notice => "Your patient has been deleted"
   end
 end
+  
+  
+  
+
+  
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,6 @@ User.destroy_all
 Event.destroy_all
 
 12.times do
-  u = User.create(email: ["frankyboss@yopmail.com", "catherinethpcode@yopmail.com", "gerardthpcode@yopmail.com", "jeanclaudethpcode@yopmail.com", "morganethpcode@yopmail.com", "michelthpcode@yopmail.com"].sample, encrypted_password: Faker::Verb.simple_present, description: Faker::Movie.quote, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+  u = User.create(email: ["frankyboss@yopmail.com", "catherinethpcode@yopmail.com", "gerardthpcode@yopmail.com", "jeanclaudethpcode@yopmail.com", "morganethpcode@yopmail.com", "michelthpcode@yopmail.com"].sample, password: Faker::Verb.simple_present, description: Faker::Movie.quote, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
   e = Event.create(start_date: Faker::Time.between(from: DateTime.now + 3, to: DateTime.now + 9, format: :default), duration: rand(1..140), title: Faker::JapaneseMedia::OnePiece.akuma_no_mi, description: Faker::JapaneseMedia::OnePiece.quote, price: rand(20..50), location: Faker::Nation.capital_city, admin_id: u.id)
 end
